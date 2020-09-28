f3c6e770d48fa07d39cdb5ae43a7782a86f959ec
Michal Sznajder <michal.a.sznajder@gmail.com>
2020-09-28 12:02:45 +0200

    better indicators

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const navLinks = document.querySelector(".nav-links");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+// const polishElems = document.querySelectorAll('body [lang="pl-PL"]');
+// const englishElems = document.querySelectorAll('body [lang="en"]');
+
+// polishElems.forEach((elem) => (elem.style.display = "none"));
